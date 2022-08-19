9b39608261cccb051e001d262b15dc22f720bb92
Grant Kinsley <grantkinsley@gmail.com>
2022-08-19 11:46:28 -0700

    exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr /*optional args*/) {
+    let numArgs = arguments.length;
+    let result = arr;
 
+    for (let k = 1; k < numArgs; k++) {
+        result = result.filter(element => element !== arguments[k]);
+        console.log(result);
+    }
+    return result;
 };
 
 // Do not edit below this line
