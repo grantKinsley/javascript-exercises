3521b5fa583d529f243a979cfe90f29241670018
Grant Kinsley <grantkinsley@gmail.com>
2022-08-19 13:49:13 -0700

    exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,10 @@
-const ftoc = function() {
-
+const ftoc = function(degf) {
+  return Math.round(((degf - 32) * (5/9)) * 10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(degc) {
+  
+  return Math.round((degc * (9/5) + 32) * 10)/10;
 };
 
 // Do not edit below this line
