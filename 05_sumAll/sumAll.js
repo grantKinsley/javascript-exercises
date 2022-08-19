9afbcac7948c42534351b461363fd167b707d65a
Grant Kinsley <grantkinsley@gmail.com>
2022-08-19 11:58:28 -0700

    exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let sum = 0;
 
+    if (typeof(int1) != 'number' || typeof(int2) != 'number') {
+        return 'ERROR';
+    }
+
+    if (int1 < 0 || int2 < 0) {
+        return 'ERROR';
+    }
+
+    let lo = int1;
+    let hi = int2;
+    if (int1 > int2) {
+        lo = int2;
+        hi = int1;
+    }
+
+    for (let k = lo; k <= hi; k++) {
+        sum += k;
+    }
+    return sum;
 };
 
 // Do not edit below this line
