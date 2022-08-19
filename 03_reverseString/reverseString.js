40aa0499a8a6579b9d29c0e73b735743746dd907
Grant Kinsley <grantkinsley@gmail.com>
2022-08-18 22:28:58 -0700

    exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let ret = '';
+    for (let k = 0; k < str.length; k++) {
+        ret = str.charAt(k) + ret;
+    }
 
+    return ret;
 };
 
 // Do not edit below this line
