7d6ae87ac978dfbe1021ffca5c68df7e8afe3fca
Grant Kinsley <grantkinsley@gmail.com>
2022-08-18 19:29:47 -0700

    exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return 'ERROR'
+    }
+    
+    ret = ''
+    for (let k = 0; k < num; k++) {
+        ret += str
+    }
+    return ret
 };
 
 // Do not edit below this line
