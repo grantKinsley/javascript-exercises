f17d8f41d71e60f05123cd8c5ce0b8ae6e58cf33
Grant Kinsley <grantkinsley@gmail.com>
2022-08-20 21:26:07 -0700

    exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const processedString = str.toLowerCase().replace(/[^a-z]/g, '');
+    // console.log(processedString);
 
+    return processedString
+            .split('')
+            .reverse()
+            .join('') === processedString;
 };
 
 // Do not edit below this line
