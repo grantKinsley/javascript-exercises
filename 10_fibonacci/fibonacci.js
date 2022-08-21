384e6452a317cd1038dfb138a716200c35ca5b58
Grant Kinsley <grantkinsley@gmail.com>
2022-08-21 16:53:46 -0700

    exercise 10: includes case 0

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,17 +1,17 @@
 const fibonacci = function(n) {
-    if (n <= 0) {
+    if (n < 0) {
         return 'OOPS';
     }
     
-    if (n == 1 || n ==2) {
-        return 1;
+    if (n == 0) {
+        return 0;
     }
     
-    let f1 = 1;
+    let f1 = 0;
     let f2 = 1;
 
     // after each iteration, f2 holds the kth fib number
-    for (let k = 3; k <= n; k++) {
+    for (let k = 2; k <= n; k++) {
         let tmp = f2;
         f2 = f1 + f2;
         f1 = tmp;
