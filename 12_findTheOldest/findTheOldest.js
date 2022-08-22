895260cc9d8310c1fb277f63a8e1a46b746364a7
Grant Kinsley <grantkinsley@gmail.com>
2022-08-21 17:20:37 -0700

    exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((a, b) => {
+        const aAge = a.yearOfDeath == undefined ?
+                        Number((new Date()).getFullYear()) - a.yearOfBirth :
+                        a.yearOfDeath - a.yearOfBirth;
+        const bAge = b.yearOfDeath == undefined ?
+                        Number((new Date()).getFullYear()) - b.yearOfBirth :
+                        b.yearOfDeath - b.yearOfBirth;
+        return aAge > bAge ? a : b;
+    }, {});
 };
 
 // Do not edit below this line
